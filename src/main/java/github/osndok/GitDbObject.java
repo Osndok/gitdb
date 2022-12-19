4cbc1907247c3615464633de382bf32ad1c280e2
Robert Hailey <git@osndok.com>
2022-12-18 19:58:05 -0600

    Sketch out Transaction and GitDbObject

---
+++ src/main/java/github/osndok/GitDbObject.java
@@ -0,0 +1,23 @@
+package github.osndok;
+
+import java.util.UUID;
+
+/**
+ * All objects to be persisted in the database must extend this class.
+ */
+public abstract
+class GitDbObject
+{
+    UUID _db_id;
+
+    /**
+     * If null, this object has not been persisted into the database; otherwise, it represents
+     * an identifier that can be used to re-fetch this object later. Once saved into a database
+     * transaction, or loaded from disk, this field will be non-null.
+     */
+    public final
+    UUID _db_id()
+    {
+        return _db_id;
+    }
+}
---
+++ src/main/java/github/osndok/Transaction.java
@@ -0,0 +1,51 @@
+package github.osndok;
+
+import java.util.UUID;
+
+public
+interface Transaction
+{
+    <T extends GitDbObject> T get(Class<T> c, UUID id);
+    <T extends GitDbObject> T get(Class<T> c, String id);
+    <T extends GitDbObject> Iterable<UUID> listUuids(Class<T> c);
+
+    /**
+     * Causes the given object to be persisted into the database.
+     */
+    void save(GitDbObject object);
+
+    void commit(String message);
+    void abort();
+
+    /**
+     * Saves the object in question, or throws an exception if it has already been saved.
+     */
+    default
+    UUID create(GitDbObject object)
+    {
+        if (object._db_id != null)
+        {
+            throw new IllegalStateException("Object has already been saved");
+        }
+
+        save(object);
+
+        var id = object._db_id;
+        assert id != null;
+        return id;
+    }
+
+    /**
+     * Saves the object in question, or throws if the object was not already saved.
+     */
+    default
+    void update(GitDbObject object)
+    {
+        if (object._db_id == null)
+        {
+            throw new IllegalStateException("Attempting to update an object that was never persisted");
+        }
+
+        save(object);
+    }
+}
