61d92cb995c103f340acd45e4f65abfb880e3813
Robert Hailey <git@osndok.com>
2022-12-18 23:19:51 -0600

    Remodel pathing scheme mechanism to allow listing classes & ids

--- src/main/java/github/osndok/PathingScheme.java
+++ src/main/java/github/osndok/PathingScheme.java
@@ -1,11 +1,30 @@
 package github.osndok;
 
+import java.io.File;
+import java.util.UUID;
+
 public
 interface PathingScheme
 {
     /**
-     * @param object - with an _db_id
-     * @return a path that the object would be located at
+     * @param repoDir - the root of the git repo
+     * @param classId
+     * @param uuid    - a uuid of a gitdb object that may or may not be present in the repo
+     * @return a path where an object with that uuid would be located, if it were in the repo
      */
-    String getObjectPath(GitDbObject object);
+    File getObjectPath(File repoDir, final String classId, UUID uuid);
+
+    <T extends GitDbObject>
+    String getClassId(File gitRepo, Class<T> c);
+
+    Iterable<String> listClassIds(File repoDir);
+
+    Iterable<UUID> listObjectIds(File repoDir, String classId);
+
+    default File getObjectPath(File repoDir, GitDbObject object)
+    {
+        var classId = getClassId(repoDir, object.getClass());
+        var uuid = object._db_id();
+        return getObjectPath(repoDir, classId, uuid);
+    }
 }
--- src/main/java/github/osndok/pathing/StupidlySimplePathing.java
+++ src/main/java/github/osndok/pathing/StupidlySimplePathing.java
@@ -3,22 +3,70 @@ package github.osndok.pathing;
 import github.osndok.GitDbObject;
 import github.osndok.PathingScheme;
 
+import java.io.File;
+import java.util.HashSet;
+import java.util.UUID;
+
 public
 class StupidlySimplePathing
         implements PathingScheme
 {
     @Override
     public
-    String getObjectPath(final GitDbObject object)
+    File getObjectPath(final File repoDir, final String classId, final UUID uuid)
     {
-        var name = object.getClass().getSimpleName();
-        var uuid = object._db_id();
-
         if (uuid == null)
         {
             throw new IllegalStateException("db object has no id assigned");
         }
 
-        return String.format("%s/%s", name, uuid);
+        var basename = String.format("%s/%s", classId, uuid);
+        return new File(repoDir, basename);
+    }
+
+    @Override
+    public
+    <T extends GitDbObject> String getClassId(final File gitRepo, final Class<T> c)
+    {
+        return c.getSimpleName();
+    }
+
+    @Override
+    public
+    Iterable<String> listClassIds(final File repoDir)
+    {
+        var dirs = new HashSet<String>();
+        for (File file : repoDir.listFiles())
+        {
+            var name = file.getName();
+
+            if (file.isDirectory() && name.indexOf('.') < 0)
+            {
+                dirs.add(name);
+            }
+        }
+
+        return dirs;
+    }
+
+    @Override
+    public
+    Iterable<UUID> listObjectIds(final File repoDir, final String classId)
+    {
+        var dir = new File(repoDir, classId);
+
+        var ids = new HashSet<UUID>();
+
+        for (File file : dir.listFiles())
+        {
+            var name = file.getName();
+
+            if (file.isFile() && name.indexOf('.') < 0)
+            {
+                ids.add(UUID.fromString(name));
+            }
+        }
+
+        return ids;
     }
 }
