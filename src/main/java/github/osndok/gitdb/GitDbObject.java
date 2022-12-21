89df1946ce3b1c191cf14f9e71ee322cabe649bd
Robert Hailey <git@osndok.com>
2022-12-21 14:04:41 -0600

    Shart id & toString() method

--- src/main/java/github/osndok/gitdb/GitDbObject.java
+++ src/main/java/github/osndok/gitdb/GitDbObject.java
@@ -21,4 +21,41 @@ class GitDbObject
     {
         return _db_id;
     }
+
+    private String _db_id_short_memo;
+
+    /**
+     * If saved to the database, this will return the first 8 characters of the UUID (the first "segment"),
+     * for more convenient (but less precise) reference to this object that, depending on the situation,
+     * may be more suitable for presentation to a human. If the object has not been persisted, this method
+     * will return null.
+     */
+    public final
+    String _db_id_short()
+    {
+        if (_db_id_short_memo == null && _db_id != null)
+        {
+            _db_id_short_memo = _db_id.toString().substring(0, 8);
+        }
+
+        return _db_id_short_memo;
+    }
+
+    @Override
+    public
+    String toString()
+    {
+        var className = getClass().getSimpleName();
+
+        if (_db_id == null)
+        {
+            return className + "{unsaved}";
+        }
+        else
+        {
+            return className + "{" +
+                   "_db_id=" + _db_id +
+                   '}';
+        }
+    }
 }
